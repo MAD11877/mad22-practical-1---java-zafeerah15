f165d3dd6b3a693de92638f95fa14bb9217809ff
zafeerah15 <97090248+zafeerah15@users.noreply.github.com>
2022-04-20 17:33:59 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -2,10 +2,10 @@ public class Question1
 {
   public static void main(String[] args)
   {
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println("Hello World!")  
-     */
+    
+     /* Print "Hello World!" */
+    
+    System.out.println("Hello World!")  
+    
   }
 }
